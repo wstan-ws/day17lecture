32a10cd7327bd273f151bc67a8e1ef4eb83a6382
wstan-ws <wstan.ws97@gmail.com>
2023-12-05 10:34:32 +0800

    initial commit

---
+++ src/main/java/vttp/iss/day17lecture/Day17lectureApplication.java
@@ -0,0 +1,13 @@
+package vttp.iss.day17lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17lectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/iss/day17lecture/Day17lectureApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.iss.day17lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17lectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
