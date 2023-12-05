72e047a637cea659d9ffa6f4cd54aea7c3c37d9e
wstan-ws <wstan.ws97@gmail.com>
2023-12-05 10:41:39 +0800

    initial commit

---
+++ src/main/java/vttp/iss/day17lecture/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package vttp.iss.day17lecture.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+
+    
+    
+}
